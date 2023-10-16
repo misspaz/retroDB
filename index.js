9bf28b9c7947c4a88070458f611a578323e93cee
misspaz <macarenapaz91@gmail.com>
2023-10-16 10:13:42 +0200

    index

--- index.js
+++ index.js
@@ -20,12 +20,10 @@ cloudinary.config({
 server.use(cors());
 
 
-const cors = require("cors")
 
-const server = express();
 
 //En este paso añadimos cors y definimos las direcciones que van a tener permiso para utilizar nuestra API. De momento en local:
-server.use(cors);
+
 
 
 
